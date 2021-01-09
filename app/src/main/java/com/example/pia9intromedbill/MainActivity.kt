23ee566e9aca09f24e6e874bd3b983f5727092a7
lina <linasigridli@gmail.com>
2021-01-09 10:40:01 +0100

    Start av projekt-har lagt till knappar, en bild

--- app/src/main/java/com/example/pia9intromedbill/MainActivity.kt
+++ app/src/main/java/com/example/pia9intromedbill/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.pia9intromedbill
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toptext: TextView = findViewById<TextView>(R.id.theTopText)
+
+        toptext.text = "Hejsan hej!!! "
+
+        val topbutton = findViewById<Button>(R.id.theTopButton)
+
+        topbutton.setOnClickListener {
+            toptext.text = "Nu händer det!!! "
+        }
     }
 }
